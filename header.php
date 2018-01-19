3159864ed63288ac42d1d687b6dd4346f6ca9296
jorisgengler <jorisgengler@hotmail.com>
2018-01-19 18:24:54 +0100

    Font in header change

--- header.php
+++ header.php
@@ -12,12 +12,12 @@
       </div><!--/slogan-->
 
       <div class="navbar" id="myNavbar">
-        <a href="index.php">Home</a>
-        <a href="de boerderij.php">DE BOERDERIJ</a>
-        <a href="producten.php">PRODUCTEN</a>
-        <a href="fresh food academy.php">FRESH FOOD ACADEMY</a>
-        <a href="recepten.php">RECEPTEN</a>
-        <a href="contact.php">CONTACT</a>
+        <a href="index.php"><p>Home</p></a>
+        <a href="de boerderij.php"><p>DE BOERDERIJ</p></a>
+        <a href="producten.php"><p>PRODUCTEN</p></a>
+        <a href="fresh food academy.php"><p>FRESH FOOD ACADEMY</p></a>
+        <a href="recepten.php"><p>RECEPTEN</p></a>
+        <a href="contact.php"><p>CONTACT</p></a>
         <a href="javascript:void(0);" style="font-size:31px;" class="icon" onclick="myFunction()">&#9776;</a>
       </div>
 
