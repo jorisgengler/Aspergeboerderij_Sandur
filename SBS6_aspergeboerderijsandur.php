703af360821c9c4abc83048c90ce41ac74ead28b
jorisgengler <jorisgengler@hotmail.com>
2018-01-21 17:14:07 +0100

    SBS6 video in youtube

---
+++ SBS6_aspergeboerderijsandur.php
@@ -0,0 +1,18 @@
+<html>
+<?php include 'head.php'; ?>
+
+<body>
+<div class="container">
+
+<?php include 'header.php';?> <!-- Roept de header op  -->
+
+
+
+<iframe id="sbs6video"
+src="https://www.youtube.com/embed/jgjx7oqzHnU">
+</iframe>
+
+</div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
