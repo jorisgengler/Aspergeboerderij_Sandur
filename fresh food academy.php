6764441536bdd6dca5e5a0fbf7368a6af2291042
jorisgengler <jorisgengler@hotmail.com>
2018-01-23 11:38:45 +0100

    fresh food academy other layout

--- fresh food academy.php
+++ fresh food academy.php
@@ -8,25 +8,35 @@
 
 <div class="gridfreshfoodacademy">
 
-  <div class="freshfoodacademy1" id="freshfoodacademy1">
-    <div class="textvlakwhitedoorzichtigfresh">
-      <h3 class="center">Fresh food academy</h3>
-    </div>
-
+  <div class="freshfoodacademyhome" id="freshfoodacademyhome"> <!-- home page freshfoodacademy -->
+    <div class="textvlakwhitedoorzichtigfresh"><h3 class="center">Fresh food academy</h3></div>
     <div class="demo">
-      <a href="#sectiontussen1"><span></span></a>
+      <a href="#sectiontussen1"><span></span></a> <!-- button -->
     </div>
-
   </div>
-    <div id="sectiontussen1" class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+
+
+
+  <div class="freshfoodacademy1">
+    <h3>Fresh food academy</h3>
+  </br>
+    <p>
+      Wij verzorgen voor bedrijven en particulieren informatieve programma‘s over vers voedsel.
+      Op een ontspannen wijze nemen we u mee in de wereld van het bewaren en verwerken van geoogste producten.
+      Deze programma‘s zijn ook geschikt als bedrijfsuitje of teambuilding.
+      Wilt u meer informatie dan kunt u een e-mail sturen.
+    </p>
+  </div>
   <div class="freshfoodacademy2">
-    <img src="images/rsz_fresh_food_academy1.jpg" style="max-width: 95%;"/>
-
-
+    <img src="images/rsz_fresh_food_academy1.jpg" style="max-width: 60%;;"/>
   </div>
-      <div id="sectiontussen2" class="sectiontussen2"></div><!-- lege tussen ruimte van 7vh -->
-  <div class="freshfoodacademy3"></div>
-  <div class="freshfoodacademy4"></div>
+  <div class="freshfoodacademy3"> </div>
+
+
+      <div id="sectiontussen1" class="sectiontussen1"></div> <!-- lege tussen ruimte van 7vh -->
+      <div id="sectiontussen2" class="sectiontussen2"></div> <!-- lege tussen ruimte van 7vh -->
+      <div id="sectiontussen3" class="sectiontussen3"></div> <!-- lege tussen ruimte van 7vh -->
+
 
 
 </div>
