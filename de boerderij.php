7f8d4288e1fbdf10311a184fe11a4bdf5584fb95
jorisgengler <jorisgengler@hotmail.com>
2018-01-24 18:49:05 +0100

    layout change

--- de boerderij.php
+++ de boerderij.php
@@ -3,91 +3,77 @@
 
 <body>
 <div class="container">
-
 <?php include 'header.php';?> <!-- Roept de header op  -->
 
-    <div class="content">
-    	<div class="tekst">
+<div class="griddeboerderij">
+  <div class="deboerderij deboerderij1">
+    <h2>De winkel</h2>
+    <p>
+    Naast onze asperges verkopen we allerlei toebehoren,
+    zodat u van iedere aspergemaaltijd een feestje kunt maken.
+    Ook bij de keuze van deze producten staat bij ons kwaliteit voorop en daarom hebben we met zorg
+    wijnen, sausjes, geschrapte krielaardappeltjes, soepen, asperge-ijs e.d. uitgekozen.
+    Onze ham komt steeds vers van de slager en de zalm van de visboer.
+    Ons assortiment blijft groeien en inmiddels verkopen we steeds meer streekproducten .
+    </p>
+  </div>
+  <div class="deboerderij deboerderij2">
+    <img  src="images/rsz_de_boerderij.jpg" alt="Boerderij" style="max-width: 95%;">
+  </div>
+  <div class="deboerderij deboerderij3">
+    <h2>De oogst</h2>
+    <p>
+    Het oogsten van onze witte asperges is puur handwerk. We halen de asperges met een speciaal steekmes uit de grond . De asperges worden na het oogsten gekoeld in fris water. Na een aantal uren worden de asperges gewassen en gesorteerd. We werken met een automatische sorteermachine, zodat de asperges nóg gedetailleerder gesorteerd kunnen worden. De mooiste asperges komen in
+    klasse I. Minder mooie in klasse II .
+    </p>
+
+  </div>
+  <div class="deboerderij deboerderij4"></div>
+  <div class="deboerderij deboerderij5">
+    <h2>Schillen</h2>
+    <p>
+    Het schillen van de asperges hoeft u niet zelf meer te doen.
+    Dankzij onze schilmachine kunt u onze asperges vers en goed geschild meenemen.
+    Zo kunt u heel snel een gezonde maaltijd op tafel zetten.
+    Wilt u de asperges een paar dagen geschild bewaren dan kunt u ze ook laten verpakken in bakjes met een speciale folie.
+    In de koelkast blijven ze nog één week houdbaar.
+    Voor degenen die een voorraad in de diepvries willen aanleggen zijn deze bakjes ook erg handig.
+    Wanneer u asperges uit de diepvries wilt gaan gebruiken wel eerst het water koken,
+    daarna de bevroren asperges in het kokende water doen.
+    De kooktijd bij asperges uit de diepvries is een minuut korter.
+    </p>
+  </div>
+  <div class="deboerderij deboerderij6"></div>
+  <div class="deboerderij deboerderij7">
+    <h2>Cadeaupakket</h2>
+    <p>
+    Wanneer u een cadeau voor iemand zoekt dan kunt u ook hiervoor bij ons terecht.
+    Wij stellen voor u een cadeaupakket samen en pakken dit mooi in.
+    Deze pakketten zijn in allerlei prijsklassen samen te stellen.
+    </p>
+  </div>
+  <div class="deboerderij deboerderij8"></div>
+  <div class="deboerderij deboerderij9">
+    <h2>Gezond</h2>
+    <p>
+    Asperges zijn erg gezond. Ze zijn rijk aan vitamines en mineralen en bevatten weinig calorieën.
+    Asperges zijn vocht afdrijvend en daarmee goed voor de nieren.
+    Van celdeling tot immuunsysteem,
+    van bloedstolling tot huid en van haar tot botten; asperges zijn goed voor het hele lijf!
+    </p>
+  </div>
+  <div class="deboerderij deboerderij10"></div>
 
 
-        <table width="535"  cellspacing="5">
-  <tr>
-    <td width="270"> <h1>De boerderij</h1>
+  <div class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen2"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen3"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen4"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen5"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen6"></div><!-- lege tussen ruimte van 7vh -->
 
+</div>
 
-<p> <br />
-Onze boerderij ligt aan de Vaart Noordzijde 186 tussen Erica en Nieuw Amsterdam.
-De
-aspergevelden liggen op zandgronden en hebben volgens onze vaste klanten een zachte,
- iets zoete
-smaak. Bepalend voor deze smaak is onder andere de manier waarop we onze asperges verzorgen.  <br /><br />
-
-
-De oogst <br /><br />
-Het oogsten van onze witte asperges is puur handwerk.  We halen de asperges met
- een speciaal
-steekmes uit de grond
-.
-De
- asperges worden na het oogsten gekoeld in fris water. Na een aantal uren
-worden de asperges gewassen en gesorteerd. We werken met een automatische sorteermachine,
-zodat de asperges n&oacuteg gedetailleerder gesorteerd kunnen worden. De mooiste asperges komen in <br />klasse I. Minder mooie in klasse II
-. <br /><br />
-Schillen <br /><br />
-Het schillen van de asperges hoeft u niet zelf meer te doen. Dankzij onze schilmachine kunt u onze
-asperges vers en goed geschild meenemen. Zo kunt u heel snel een gezonde maaltijd
- op tafel zetten.
-Wilt u de asperges een paar dagen geschild bewaren dan kunt u ze ook laten verpakken
-in bakjes met
-een speciale folie. In de koelkast blijven ze nog &eacute&eacuten week houdbaar. Voor degenen die een voorraad
-in de diepvries willen aanleggen zijn deze bakjes ook erg handig. Wanneer u asperges uit de diepvries
-wilt gaan gebruiken wel eerst het water koken, daarna de bevroren asperges in het kokende water
-doen. De kooktijd bij asperges uit de diepvries is een minuut korter.<br /><br />
-De winkel <br /><br />
-Naast onze asperges verkopen we allerlei toebehoren, zodat u van iedere aspergemaaltijd een
-feestje kunt maken. Ook bij de keuze van deze producten staat bij ons kwaliteit voorop en daarom
-hebben we met zorg wijnen, sausjes, geschrapte krielaardappeltjes, soepen, asperge-ijs e.d.
-uitgekozen. Onze ham komt steeds vers van de slager en de zalm van de visboer. Ons assortiment
-blijft groeien en inmiddels verkopen we steeds meer streekproducten
-. <br /><br />
-Cadeaupakket<br /><br />
-Wanneer u een cadeau voor iemand zoekt dan kunt u ook hiervoor bij ons terecht. Wij stellen voor u
-een cadeaupakket samen en pakken dit mooi in. Deze pakketten zijn in allerlei prijsklassen samen te
-stellen. <br /><br />
-Gezond <br /><br />
-Asperges zijn erg gezond. Ze zijn rijk aan vitamines en mineralen en bevatten weinig calorie&eumln.
-Asperges zijn vocht afdrijvend en daarmee goed voor de nieren. Van celdeling tot immuunsysteem,
-van bloedstolling tot huid en van haar tot botten; asperges zijn goed voor het hele lijf!
-</p><br />
-
-&quot;Dit bedrijf is aangesloten bij Telersvereniging ZON, de verkoop van onze producten geschiedt via ZON fruit & vegetables. &quot;
-
-<br /><br /><br />
-
-<img src="images/foto10_thumbnail.jpg" width="200" height="100
-" margin-left="50
-" />
-
-<img src="images/foto11_thumbnail.jpg" width="100" height="100
-" />
-
-  </tr>
-</table>
-
-
-
-        </div><!--/tekst-->
-
-        <div class="photosideshadow">
-            <div class="photoside">
-                <h2>De boerderij</h2>
-
-  <img src="images/de boerderij.jpg" width="200" height="133" HSPACE="18"/>              <br /><br />
-<img src="images/informatie2.jpg" width="200" height="133" HSPACE="18"/>
-
-            </div><!--/photoside>
-        </div><!--/photosideshadow-->
-    </div><!--/content-->
 </div><!--/container-->
 
 <?php include 'footer.php'; ?>
