52a925eb327ac341bfbe1222a307a47eaff28113
jorisgengler <jorisgengler@hotmail.com>
2018-01-24 18:47:10 +0100

    remove excursie add excursies

---
+++ excursies.php
@@ -0,0 +1,58 @@
+<html>
+<?php include 'head.php'; ?>
+
+<body>
+<div class="container">
+
+<?php include 'header.php';?> <!-- Roept de header op  -->
+
+<div class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+ <div class="flex-container">
+
+   <div class="arrangement">
+     <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
+    <div class="textareaexcursies">
+       <h2>Arrangementen</h2>
+      </br>
+      <p>
+        De arrangementen zijn te boeken tijdens het aspergeseizoen (eind april t/m eind juni) op de woensdag en zaterdag.
+      </p>
+    </div>
+  </div>
+<div class="arrangement">
+<h2>Arrangement 1:</h2>
+Excursie over de teelt van asperges + Verse aspergesoep.
+<br><br>
+Op de Aspergeboerderij wordt u de mogelijkheid geboden zelf uw asperges te steken en wordt u alles over de teelt van asperges verteld. Een videopresentatie, een rondleiding in en op het bedrijf en tips voor de toebereiding van asperges behoren tot een volmaakt bezoek. In het winkeltje kunt u van alles kopen wat met asperges te maken heeft. Na afloop kunt u nagenieten onder het genot van een ambachtelijke verse aspergesoep.
+Prijs €7,50 voor volwassenen. kinderen t/m 12 jaar gratis.
+
+</div>
+<div class="arrangement">
+  <h2>Arrangement 2:</h2>
+  Excursie over de teelt van asperges + Smalspoormuseum
+  <br><br>
+  Excursie over de teelt van asperges
+  Op de Aspergeboerderij wordt u de mogelijkheid geboden zelf uw asperges te steken en wordt u alles over de teelt van asperges verteld. Een videopresentatie, een rondleiding in en op het bedrijf en tips voor de toebereiding van asperges behoren tot een volmaakt bezoek. In het winkeltje kunt u van alles kopen wat met asperges te maken heeft. Na afloop kunt u nagenieten onder het genot van een ambachtelijke verse aspergesoep.
+  <br><br>
+
+</div>
+
+  <div class="arrangement">
+  <h2>Smalspoormuseum</h2>
+  Het smalspoort op zijn mooist in een natuurlijke omgeving! Maak een rit door een prachtig gebied, grotendeels over een origineel traject, vroeger gebruikt voor turftransport.
+  Tijdens de rit wordt regelmatig gestopt en uitleg gegeven over het smalspoor en het specifieke gebied.
+  Door de geringe snelheid, zo kenmerkend voor dit smalspoor, heeft u voldoende tijd om van de omgeving en de treinreis te genieten.
+  Langs het traject staat smalspoor materieel opgesteld in diverse samenstellingen en toepassingen.
+  Prijs €22,00 per persoon.
+  </div>
+
+
+
+</div>  <!--grid flex-->
+
+
+</div><!--/container-->
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
