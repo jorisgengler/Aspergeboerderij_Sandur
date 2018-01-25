b64ae7c225d5f5f4e97f3d6b9f5df0c980bd6f70
jorisgengler <jorisgengler@hotmail.com>
2018-01-25 16:44:28 +0100

    Layout change excursies

--- excursies.php
+++ excursies.php
@@ -6,33 +6,59 @@
 
 <?php include 'header.php';?> <!-- Roept de header op  -->
 
-<div class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+
  <div class="flex-container">
 
 <!-- __________________________arrangement1__________________________ -->
    <div class="arrangement">
-     <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
+     <div class="buttonplacedown">
 
-    <div class="textareaexcursies">
-       <h2>Arrangement 1:</h2>
-      </br>
-      <p>
-        De arrangementen zijn te boeken tijdens het aspergeseizoen (eind april t/m eind juni) op de woensdag en zaterdag.
-      </p>
-    </div><!-- /textareaexcursies -->
+  <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
+
+<div class="textareaexcursies">
+<h2>Arrangement 1:</h2>
+<br>
+<p4>
+<li>Excursie over de teelt van asperges</li>
+<li>Smalspoormuseum</li>
+<li>Maak een rit door een prachtig natuur gebied.</li>
+<li>Koffie/théé</li>
+</p4>
+</div><!-- /textareaexcursies -->
+
+   <div class="buttonplace">
+     <div class="prijsfont"><p>€21,95</p></div>
+     <div class="onderprijs"><p>vanaf prijs, p.p. </p></div>
+     <a href="contact.php"><div class="arrangementbutton buttoncolorgreen"> <p>Meer +</p></div></a>
+   </div>
+ <br>
+</div><!-- /buttonplacedown -->
   </div>
 
 <!-- __________________________arrangement2__________________________ -->
 <div class="arrangement">
+        <div class="buttonplacedown">
+
      <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
 
 <div class="textareaexcursies">
   <h2>Arrangement 1:</h2>
-  Excursie over de teelt van asperges + Verse aspergesoep.
-  <br><br>
-  Op de Aspergeboerderij wordt u de mogelijkheid geboden zelf uw asperges te steken en wordt u alles over de teelt van asperges verteld. Een videopresentatie, een rondleiding in en op het bedrijf en tips voor de toebereiding van asperges behoren tot een volmaakt bezoek. In het winkeltje kunt u van alles kopen wat met asperges te maken heeft. Na afloop kunt u nagenieten onder het genot van een ambachtelijke verse aspergesoep.
-  Prijs €7,50 voor volwassenen. kinderen t/m 12 jaar gratis.
+  <br>
+<p4>
+<li>Excursie over de teelt van asperges</li>
+<li>Smalspoormuseum</li>
+<li>Maak een rit door een prachtig natuur gebied.</li>
+<li>Koffie/théé</li>
+</p4>
  </div><!-- /textareaexcursies -->
+
+      <div class="buttonplace">
+        <div class="prijsfont"><p>€21,95</p></div>
+        <div class="onderprijs"><p>vanaf prijs, p.p. </p></div>
+        <a href="contact.php"><div class="arrangementbutton buttoncolorgreen"> <p>Meer +</p></div></a>
+      </div>
+    <br>
+  </div><!-- /buttonplacedown -->
 </div>
 
 <!-- __________________________arrangement3__________________________ -->
@@ -51,11 +77,11 @@
 <li>Koffie/théé</li>
 </p4>
 
-<p>Prijs €22,00</p>
-
     </div><!-- /textareaexcursies -->
 
     <div class="buttonplace">
+      <div class="prijsfont"><p>€21,95</p></div>
+      <div class="onderprijs"><p>vanaf prijs, p.p. </p></div>
       <a href="contact.php"><div class="arrangementbutton buttoncolorgreen"> <p>Meer +</p></div></a>
     </div>
     <br>
@@ -65,24 +91,35 @@
 
 <!-- __________________________arrangement4__________________________ -->
   <div class="arrangement">
-       <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
+    <div class="buttonplacedown">
+
+ <img  src="images/rsz_excursies.jpg" alt="Boerderij" style="max-width: 100%;">
 
 <div class="textareaexcursies">
-  <h2>Smalspoormuseum</h2>
-  Het smalspoort op zijn mooist in een natuurlijke omgeving! Maak een rit door een prachtig gebied, grotendeels over een origineel traject, vroeger gebruikt voor turftransport.
-  Tijdens de rit wordt regelmatig gestopt en uitleg gegeven over het smalspoor en het specifieke gebied.
-  Door de geringe snelheid, zo kenmerkend voor dit smalspoor, heeft u voldoende tijd om van de omgeving en de treinreis te genieten.
-  Langs het traject staat smalspoor materieel opgesteld in diverse samenstellingen en toepassingen.
-  Prijs €22,00 per persoon.
+<h2>Arrangement 1:</h2>
+<br>
+<p4>
+<li>Excursie over de teelt van asperges</li>
+<li>Smalspoormuseum</li>
+<li>Maak een rit door een prachtig natuur gebied.</li>
+<li>Koffie/théé</li>
+</p4>
+</div><!-- /textareaexcursies -->
 
- </div><!-- /textareaexcursies -->
+  <div class="buttonplace">
+    <div class="prijsfont"><p>€21,95</p></div>
+    <div class="onderprijs"><p>vanaf prijs, p.p. </p></div>
+    <a href="contact.php"><div class="arrangementbutton buttoncolorgreen"> <p>Meer +</p></div></a>
+  </div>
+<br>
+</div><!-- /buttonplacedown -->
 </div>
 
 
 
 </div>  <!--grid flex-->
 
-
+<div class="sectiontussen3"></div><!-- lege tussen ruimte van 7vh -->
 </div><!--/container-->
 
 <?php include 'footer.php'; ?>
