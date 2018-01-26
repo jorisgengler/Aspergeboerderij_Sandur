fa7003989f7fe95ce18b701475758bdc4d8310ba
jorisgengler <jorisgengler@hotmail.com>
2018-01-26 18:00:38 +0100

    zelf het veld in layout Sign Up change

--- zelf_het_veld_in.php
+++ zelf_het_veld_in.php
@@ -40,11 +40,11 @@
           <form  action="action_page.php" method="post">
 <br>
             <label for="firstname"> </label>
-            <input type="text" style="width: 60%;" id="firstname" name="firstname" placeholder="Uw naam">
+            <input type="text" class="reserveren" style="width: 70%;" id="firstname" name="firstname" placeholder="Uw naam">
 <br>
 <br>
             <label for="email">  </label>
-            <input type="text" style="width: 60%;" id="email" name="email" placeholder="Uw E-mail">
+            <input type="text" class="reserveren" style="width: 70%;" id="email" name="email" placeholder="Uw E-mail">
 <br>
 <br>
             <select name="maand" class="reserveren">
@@ -66,20 +66,16 @@
 <br>
 <br>
 
-<select id="maand" name="maand" class="reserveren">
-  <option value="Selecteer tijd">Selecteer datum</option>
-  <option value="15:00-16:00">15:00-16:00</option>
-  <option value="16:00-17:00">16:00-17:00</option>
-  <option value="17:00-18:00">17:00-18:00</option>
-  <option value="18:00-19:00">18:00-19:00</option>
-  <option value="20:00-21:00">20:00-21:00</option>
-  <option value="22:00-23:00">22:00-23:00</option>
-</select>
+
+
+<input id="maand" name="maand" class="reserveren" type="date">
+
+
 
 <br>
 <br>
             <label for="subject"> <p1></p1> </label>
-            <textarea id="subject" style="width: 60%;" name="subject" placeholder="Bijzonderheden" /*style="height:130px"*/></textarea>
+            <textarea id="subject" class="reserveren" name="subject" placeholder="Bijzonderheden" /*style="height:130px"*/></textarea>
 <br>
 <br>
             <input type="submit" value="Verzend">                      </input>
