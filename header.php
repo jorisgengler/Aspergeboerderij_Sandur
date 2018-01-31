a9b63c34dfae44874772ed3fa93b85fc0e9acde5
jorisgengler <jorisgengler@hotmail.com>
2018-01-31 22:33:19 +0100

    underscore filename

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
 
       <div class="navbar" id="myNavbar">
         <a href="index.php"><p>Home</p></a>
-        <a href="de boerderij.php"><p>DE BOERDERIJ</p></a>
+        <a href="de_boerderij.php"><p>DE BOERDERIJ</p></a>
         <a href="producten.php"><p>PRODUCTEN</p></a>
         <a href="fresh food academy.php"><p>FRESH FOOD ACADEMY</p></a>
-        <a href="excursies.php"><p>excursies</p></a>        
+        <a href="excursies.php"><p>excursies</p></a>
         <a href="recepten.php"><p>RECEPTEN</p></a>
         <a href="contact.php"><p>CONTACT</p></a>
         <a href="javascript:void(0);" style="font-size:31px;" class="icon" onclick="myFunction()">&#9776;</a>
