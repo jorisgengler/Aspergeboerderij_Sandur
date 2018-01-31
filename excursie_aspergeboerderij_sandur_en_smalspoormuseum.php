8ad529734d1d128445b71a26f5251828836f8a14
jorisgengler <jorisgengler@hotmail.com>
2018-01-31 22:31:32 +0100

    add excursie

---
+++ excursie_aspergeboerderij_sandur_en_smalspoormuseum.php
@@ -0,0 +1,106 @@
+<html>
+<?php include 'head.php'; ?>
+
+<body>
+<div class="container">
+<?php include 'header.php';?> <!-- Roept de header op  -->
+
+<div class="excursiepaginagrid">
+<div class="excursie1">
+  <h3>Excursie Aspergeboerderij Sandur + Smalspoormuseum</h3>
+  <p6>
+    <br>
+    <li>Excursie over de teelt van asperges</li>
+    <li>Ambachtelijke verse aspergesoep</li>
+    <li>Smalspoormuseum</li>
+    <li>Lunch in het Smalspoor Museum</li>
+  </p6>
+<br>
+<br>
+  <p6>
+    <h2>Aspergeboerderij Sandur</h3>
+    Excursie over de teelt van asperges.
+    Op de Aspergeboerderij wordt u de mogelijkheid geboden zelf uw asperges te steken en wordt u alles over de teelt van asperges verteld. Een videopresentatie,
+    een rondleiding in en op het bedrijf en tips voor de toebereiding van asperges behoren tot een volmaakt bezoek.
+    In het winkeltje kunt u van alles kopen wat met asperges te maken heeft.
+    Na afloop kunt u nagenieten onder het genot van een ambachtelijke verse aspergesoep.
+<br><br>
+    <h2>Smalspoormuseum</h2>
+    Het smalspoor op zijn mooist in een natuurlijke omgeving!
+    Maak een rit door een prachtig gebied, grotendeels over een origineel traject, vroeger gebruikt voor turftransport.
+    Tijdens de rit wordt regelmatig gestopt en uitleg gegeven over het smalspoor en het specifieke gebied.
+    Door de geringe snelheid, zo kenmerkend voor dit smalspoor, heeft u voldoende tijd om van de omgeving en de treinreis te genieten.
+    Langs het traject staat smalspoor materieel opgesteld in diverse samenstellingen en toepassingen.
+    <br>Lunch in het Smalspoor Museum: 2 broodjes, krentenbrood, koffie/thee/karnemelk.
+    <br><br>
+          Voor het reserveren van uw bezoek kunt u een e-mail sturen naar:
+    <br><br>
+    info@aspergeboerderijsandur.nl  &nbsp;&nbsp;&nbsp;&nbsp;        tel: 0591-554375
+  </p6>
+
+
+</div>
+<div class="excursie2">
+<!--
+    <h1>Reserveren</h1>
+        <div class="contactveld">
+
+          <form  action="action_page.php" method="post">
+<br>
+            <label for="firstname"> </label>
+            <input type="text" class="reserveren" style="width: 70%;" id="firstname" name="firstname" placeholder="Uw naam">
+<br>
+<br>
+            <label for="email">  </label>
+            <input type="text" class="reserveren" style="width: 70%;" id="email" name="email" placeholder="Uw E-mail">
+<br>
+<br>
+            <select name="maand" class="reserveren">
+              <option value="aantal personen">aantal personen</option>
+              <option value="1 personen">1 persoon</option>
+              <option value="2 personen">2 personen</option>
+              <option value="3 personen">3 personen</option>
+              <option value="4 personen">4 personen</option>
+              <option value="5 personen">5 personen</option>
+              <option value="6 personen">6 personen</option>
+              <option value="7 personen">7 personen</option>
+              <option value="8 personen">8 personen</option>
+              <option value="9 personen">9 personen</option>
+              <option value="10 personen">10 personen</option>
+              <option value="11 personen">11 personen</option>
+              <option value="12 personen">12 personen</option>
+              <option value="Meer personen">Meer personen graag vermelden in bijzonderheden</option>
+            </select>
+<br>
+<br>
+
+
+
+<input id="maand" name="maand" class="reserveren" type="date">
+
+
+
+<br>
+<br>
+            <label for="subject"> <p1></p1> </label>
+            <textarea id="subject" class="reserveren" name="subject" placeholder="Bijzonderheden" /*style="height:130px"*/></textarea>
+<br>
+<br>
+            <input type="submit" value="Verzend">                      </input>
+          </form>
+        </div> -->
+
+</div> <!--/excursie2-->
+
+
+
+
+<div class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+<div class="sectiontussen2"></div><!-- lege tussen ruimte van 7vh -->
+
+</div><!--/excursiepaginagrid-->
+</div><!--/container-->
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
