c27440ac33bea832a2de744caef81f338c9e515c
jorisgengler <jorisgengler@hotmail.com>
2018-02-02 21:07:49 +0100

    change layout recept

--- pittigepeperaspergesoep.php
+++ pittigepeperaspergesoep.php
@@ -6,39 +6,51 @@
 
 <?php include 'header.php';?> <!-- Roept de header op  -->
 
-    <div class="content">
-    	<div class="tekst">
-        	<h1>Pittige pepersoep met asperges</h1>
-<p><i>Ingrediënten:</i></p>
-<p>·         8 dikke witte asperges</p>
-<p>·         2 rode pepertjes, gehalveerd en zaadlijsten verwijderd</p>
-<p>·         50 g boter</p>
-<p>·         1 ui, fijngesnipperd</p>
-<p>·         2 teentjes knoflook, fijngehakt</p>
-<p>·         50 g bloem</p>
-<p>·         250 ml gezeefde tomaten</p>
-<p>·         100 ml slagroom of crème fraiche</p>
-<p>·         peper en zout </p><br />
+<div class="gridrecepten">
 
-<p><i>Bereiden:</i></p>
-<p>Schil de asperges. Doe één pepertje, asperges, asperge-uiteinden en schillen (gewassen!) in een pan en zet dit net onder water met  eventueel een beetje zout. Breng dit aan de kook en haal na 6 minuten de asperges eruit. Laat de rest nog 30 minuten zachtjes trekken. Hierna zeven en het kookvocht bewaren. Hak het resterende pepertje fijn. Smelt de boter en bak de ui met de knoflook en pepertje 2-3 minuten. Roer de bloem erdoor en bak deze 1 minuut mee om te garen. Schenk de gezeefde tomaten erbij en roer tot een licht- gebonden en egale soep. Schenk er 500 ml aspergekookvocht en de slagroom of crème fraiche bij en roer tot een glad mengsel. Voeg eventueel nog zoveel aspergevocht toe tot de soep de gewenste dikte heeft bereikt. Breng de soep op smaak met peper en zout. Verdeel de stukken asperges over 4 borden of koppen en bestrooi eventueel met fijngehakte rode peper.</p><br />
+  <div class="meerrecepten"></div>
 
-<p>Smakelijk eten!     </p>
-<p>Bron: Asperges van Francis van Arkel (Veltman Uitgevers)</p>
+  <div class="recept1">
+    <h1>Pittige pepersoep met asperges</h1>
+        <br>
+    <img src="images/foto7_thumbnail.jpg" style="width: 50%;">
+  </div>
+  <div class="recept2">
+<p5>
+  Bereidingstijd: 20 min.<br>
+  Aantal personen: 4
+</p5>
+<br><br>
+    <p4>
+    Ingrediënten:<br><br>
 
-</div><!--/tekst-->
+    <li>8 dikke witte asperges</li>
+    <li>2 rode pepertjes, gehalveerd en zaadlijsten verwijderd</li>
+    <li>50 g boter</li>
+    <li>1 ui, fijngesnipperd</li>
+    <li>2 teentjes knoflook, fijngehakt</li>
+    <li>50 g bloem</li>
+    <li>250 ml gezeefde tomaten</li>
+    <li>100 ml slagroom of crème fraîche</li>
+    <li>peper en zout</li>
 
-        <div class="photosideshadow">
-            <div class="photoside">
-            <h1>Pittige pepersoep met asperges</h1>
-              <p>Bereidingstijd: 20 min.</p>
-              <p>Aantal personen: 4</p>
-                <div class="photoborder">
-                    <img src="images/foto7_thumbnail.jpg" width="220" height="161" />
-                </div><!--/photoborder-->
-            </div><!--/photoside>
-        </div><!--/photosideshadow-->
-    </div><!--/content-->
+  </p4>
+
+  </div>
+  <div class="recept3">
+    <h1>Bereiden:</h1>
+    <p4>
+    Schil de asperges. Doe één pepertje, asperges, asperge-uiteinden en schillen (gewassen!) in een pan en zet dit net onder water met eventueel een beetje zout. Breng dit aan de kook en haal na 6 minuten de asperges eruit. Laat de rest nog 30 minuten zachtjes trekken. Hierna zeven en het kookvocht bewaren. Hak het resterende pepertje fijn. Smelt de boter en bak de ui met de knoflook en pepertje 2-3 minuten. Roer de bloem erdoor en bak deze 1 minuut mee om te garen. Schenk de gezeefde tomaten erbij en roer tot een licht gebonden en egale soep. Schenk er 500 ml aspergekookvocht en de slagroom of crème fraîche bij en roer tot een glad mengsel. Voeg eventueel nog zoveel aspergevocht toe tot de soep de gewenste dikte heeft bereikt. Breng de soep op smaak met peper en zout. Verdeel de stukken asperges over 4 borden of koppen en bestrooi eventueel met fijngehakte rode peper.
+    Smakelijk eten!
+    Bron: Asperges van Francis van Arkel (Veltman Uitgevers)
+  </p4>
+
+  </div>
+
+  <div class="sectiontussen1"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen2"></div><!-- lege tussen ruimte van 7vh -->
+  <div class="sectiontussen3"></div><!-- lege tussen ruimte van 7vh -->
+</div><!--/Grid recepten-->
 </div><!--/container-->
 
 <?php include 'footer.php'; ?>
