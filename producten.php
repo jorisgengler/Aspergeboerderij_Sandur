4fc07c96d1703a68d48826c098dfa0c42f338204
jorisgengler <jorisgengler@hotmail.com>
2018-02-04 15:11:46 +0100

    layout producten

--- producten.php
+++ producten.php
@@ -6,50 +6,37 @@
 
 <?php include 'header.php';?> <!-- Roept de header op  -->
 
-    <div class="content">
-    	<div class="tekst">
+<div class="gridproducten">
+  <div class="producten1">
+    <h3>Producten</h3>
+    <br>
+    <a href="/images/rsz_winkel.jpg"><img src="images/rsz_winkel.jpg" alt="Winkel" style="max-width: 95%;"> </a>
+  </div>
+  <div class="producten2">
+    <p>
+      In onze boerderijwinkel kunt u van april tot eind juni terecht voor de lekkerste Drentse asperges.
+      U kunt de asperges bij ons laten schillen.
+      <br><br>
+      Naast de asperges vindt u bij ons allerlei bijpassende producten, zoals lekkere geschrapte krielaardappeltjes, eieren, ham, zalm, wijn en aspergebier.
+      Onze ham komt steeds vers van de slager en de zalm van de visboer.
+      Daarnaast hebben we een groot assortiment aan aspergeschalen, kommen en aspergeschillers.
+      <br><br>
+      Ook voor allerlei streekproducten kunt u bij ons terecht, bijvoorbeeld kaas, jam, nootjes, chips en lekkere sapjes.
+      Ons assortiment blijft groeien en inmiddels verkopen we steeds meer streekproducten.
+      Bij de keuze van deze producten staat bij ons kwaliteit voorop.
+      <br><br>
+      Kortom een winkel waar u terecht kunt om van uw maaltijd een feestje te maken!
+      Wilt u graag eens zien hoe het telen van asperges in zijn werk gaat?
+      Kom dan eens langs voor een rondleiding op ons bedrijf.
+      We laten u met plezier onze aspergevelden en de boerderij zien.
+    </p>
+  </div>
+  <div class="producten3">
+
+  </div>
 
 
-        <table width="535"  cellspacing="5">
-  <tr>
-    <td width="270"> <h1>Producten</h1>
-
-
-<p>Naast (geschilde) asperges verkopen wij ook de volgende artikelen: <br/><br />
-aspergesaus en -soep; <br />
-krielaardappels;<br />
-ham;<br />
-zalm;<br />
-eieren;<br />
-aspergewijn; <br />
-aspergelikeur; <br />
-aspergeschillers; <br />
-aspergeservetten; <br />
-aspergepannen en schalen; <br />
-kookboeken met aspergerecepten; <br />
-handdoeken en theedoeken met aspergemotief;<br />
-aspergeserviezen;<br />
-streekproducten, o.a. Drentsgoed;<br />
-aardbeien;<br />
-aspergebier;<br />
-asperge-ijs.</td>
-
-
-</table>
-
-
-
-        </div><!--/tekst-->
-
-        <div class="photosideshadow">
-            <div class="photoside">
-                <h2>Producten</h2>
-                <img src="images/producten1.jpg" width="200" height="133" HSPACE="18"/>     <br /><br />
-   <img src="images/producten2.jpg" width="200" height="133" HSPACE="18"/>
-
-            </div><!--/photoside>
-        </div><!--/photosideshadow-->
-    </div><!--/content-->
+  </div><!-- /End Grid -->
 </div><!--/container-->
 
 <?php include 'footer.php'; ?>
